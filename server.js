5bb72d99d37a39aacb1fe5bc7d60ced18345f9b5
anniejenifer557 <anniejenifer0507@gmail.com>
2026-03-26 19:59:03 +0530

    fix: correct cors ver

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 500
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
