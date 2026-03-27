181f8a3c75f93643f2c5d59a7010c64748a30209
Anniejenifer557 <Anniejenifer557@users.noreply.github.com>
2026-03-27 07:43:49 +0000

    deploy: daf2028d4afbbe29223df54f7a03f11db923db04

--- server.js
+++ server.js
@@ -2,16 +2,42 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
+mongoose.connect("mongodb+srv://anniejenifer0507_db_user:<db_password>@cluster0.xs8flvx.mongodb.net/?appName=Cluster0")
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log(err));
 const app = express();
 
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json());const feedbackSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String
+});
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+
+// ✅ POST ROUTE
+app.post("/feedback", async (req, res) => {
+  console.log("DATA RECEIVED:", req.body);
+
+  try {
+    const newFeedback = new Feedback(req.body);
+    await newFeedback.save();
+
+    res.send("Saved ✅");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error saving data");
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const PORT = process.env.PORT || 500
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
