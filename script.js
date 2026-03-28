1c11fd8a4532d0299761b46e6d4474584bf5054c
anniejenifer557 <anniejenifer0507@gmail.com>
2026-03-28 18:36:07 +0530

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-document.addEventListener("DOMContentLoaded",function () {
-document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+window.onload = function() {
+  document.getElementById("feedbackForm").addEventListener("submit", function(event) {
     event.preventDefault();
+    event.stopPropagation();
 
     const nameValue = document.getElementById("fname").value;
     const emailValue = document.getElementById("femail").value;
     const messageValue = document.getElementById("ffeedback").value;
 
-    fetch("https://anniejenifer-portfolio-project-2.onrender.com/feedback", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name: nameValue, email: emailValue, message: messageValue })
+    fetch("http://127.0.0.1:5000/feedback", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: nameValue, email: emailValue, message: messageValue })
     })
     .then(res => res.text())
     .then(data => {
-        alert(data);
-    document.getElementById("feedbackForm").reset();
+      document.getElementById("modal").style.display = "flex";
+      document.getElementById("feedbackForm").reset();
     })
-    .catch(err => { console.log(err); 
-        alert("Error ❌"); });
-});
+    .catch(err => {
+      console.log(err);
+      alert("Error ❌");
+    });
+  });
+};
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
